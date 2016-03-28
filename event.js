64b8fc1dd4db91e24c75634a047499ad721d44e2
zhun <zhun@huoli.com>
2016-03-28 19:02:04 +0800

    event order

--- event.js
+++ event.js
@@ -4,16 +4,18 @@ function onload() {
 	var em2 = document.getElementById('div2');
 	var em3 = document.getElementById('div3');
 
+	em1.addEventListener('click', clickFun, true);
+	em2.addEventListener('click', clickFun, true);
+	em3.addEventListener('click', clickFun, true);
+
 	em1.addEventListener('click', clickFun, false);
-	// em3.addEventListener('click', function(e) {
-	// 	e.stopPropagation();
-	// 	alert("this is div3's own alert" + ' ' + e.target.id +' ' + e.currentTarget.id);
-	// }, false);
+	em2.addEventListener('click', clickFun, false);
+	em3.addEventListener('click', clickFun, false);
 }
 
 
 function clickFun(e) {
-	e.target.style.backgroundColor='blue';
+	e.currentTarget.style.backgroundColor='blue';
 	alert(e.target.id +' ' + e.currentTarget.id);
-	e.target.style.backgroundColor='';
+	e.currentTarget.style.backgroundColor='';
 } 
