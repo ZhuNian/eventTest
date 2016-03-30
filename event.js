6c81955f6ca5d98eba3a5f193304f718b6e78904
zhun <zhun@huoli.com>
2016-03-30 09:16:29 +0800

    prevent default

--- event.js
+++ event.js
@@ -4,13 +4,25 @@ function onload() {
 	var em2 = document.getElementById('div2');
 	var em3 = document.getElementById('div3');
 
-	em2.addEventListener('click', function(e){
-		// e.preventDefault();
-		alert('div2 prevent default');
+	em2.addEventListener('keypress', function(evt){
+		var charCode = evt.charCode;
+	    if (charCode != 0) {
+	        if (charCode < 97 || charCode > 122) {
+	            evt.preventDefault();
+	            alert(
+	                "Please use lowercase letters only."
+	                + "\n" + "charCode: " + charCode + "\n"
+	            );
+	        }
+	    }
 	}, false);
 
 
-	em3.addEventListener('click', function(){
-		alert('div3');
+
+
+	em3.addEventListener('keypress', function(){
+	}, false);
+
+	em1.addEventListener('keypress', function(){
 	}, false);
 }
